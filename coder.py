351a975e6ca4c2db055666fee7bdb1f33bfcb7f9
Nikita Pustovoit <52323913+SpoiledBogdan@users.noreply.github.com>
2021-11-20 16:54:27 +0300

    Update coder.py
    Added a color text with colorama lib

--- coder.py
+++ coder.py
@@ -1,4 +1,8 @@
 import math
+import colorama
+from colorama import Fore, Style
+
+colorama.init()
 
 
 def dec_to_bin():
@@ -6,12 +10,15 @@ def dec_to_bin():
     # validating user input
     while True:
         try:
-            dec = int(input("Type a dec number: "))
+            dec = int(input(Fore.LIGHTYELLOW_EX + "Type a dec number: "))
+            print(Style.RESET_ALL)
+
             string = ''
             if isinstance(dec, bool):   # if dec is not int
                 raise ValueError
         except ValueError:
-            print("You should type a dec number! ")
+            print(Fore.RED + "You should type a dec number! ")
+            print(Style.RESET_ALL)
         else:
             break
 
@@ -20,7 +27,8 @@ def dec_to_bin():
         string += str(bin_)
         dec = dec / 2
 
-    print(string[::-1])
+    print(Fore.GREEN + "Dec to Bin result: " + string[::-1])
+    print(Style.RESET_ALL)
 
 
 def bin_to_dec():
@@ -29,14 +37,17 @@ def bin_to_dec():
     # validating user input
     while True:
         try:
-            bin_int = int(input("Type a binary number: "))
+            bin_int = int(input(Fore.LIGHTYELLOW_EX + "Type a binary number: "))
+            print(Style.RESET_ALL)
+
             bit_list = list(str(bin_int))
 
             for i in list(str(bin_int)):
                 if i != '1' and i != '0':
                     raise ValueError
         except ValueError:
-            print("You should type a binary number! ")
+            print(Fore.RED + "You should type a binary number! ")
+            print(Style.RESET_ALL)
         else:
             break
 
@@ -48,12 +59,16 @@ def bin_to_dec():
         else:
             bit_list[i] = (2 ** (size - i - 1))
             dec += bit_list[i]
-    print(dec)
+    print(Fore.GREEN + "Bin to Dec result: ", dec)
+    print(Style.RESET_ALL)
 
 
 def xor_cipher():
-    message = list(str(input("Type a message: ")))
-    key = list(str(input("Type a key: ")))
+    message = list(str(input(Fore.LIGHTYELLOW_EX + "Type a message: ")))
+    print(Style.RESET_ALL)
+
+    key = list(str(input(Fore.LIGHTYELLOW_EX + "Type a key: ")))
+    print(Style.RESET_ALL)
 
     message_list = []
     key_list = []
@@ -82,22 +97,34 @@ def xor_cipher():
     # encode result
     for i in range(len(message)):
         encode_result.append(int(message_list[i]) ^ int(key_list[i]))
-    print("Encode message: ", encode_result)    # I have to print a list of numbers, not an array of numbers
+    print(Fore.GREEN + "Encode message: ", encode_result)   # I have to print a list of numbers, not an array of numbers
+    print(Style.RESET_ALL)
 
     # decode result
     for i in range(len(encode_result)):
         decode_result.append(chr(encode_result[i] ^ key_list[i]))
-    print("Decode message: ", ''.join(decode_result))
+    print(Fore.GREEN + "Decode message: ", ''.join(decode_result))
+    print(Style.RESET_ALL)
 
 
 while True:
-    char_input = int(input("Enter the number for the command:\n1. Dec to bin\n2. Bin to dec\n3. Xor cipher\n0. Exit\n"))
-    
-    if char_input == 1:
-        dec_to_bin()
-    elif char_input == 2:
-        bin_to_dec()
-    elif char_input == 3:
-        xor_cipher()
-    elif char_input == 0:
-        break
+    try:
+
+        char_input = int(input(Fore.LIGHTBLUE_EX + "Enter the number for the command:\n"
+                               + Fore.BLUE + "1. Dec to bin\n"
+                               + Fore.LIGHTBLUE_EX + "2. Bin to dec\n"
+                               + Fore.BLUE + "3. Xor cipher\n\n"
+                               + Fore.LIGHTBLUE_EX + "0. Exit\n"))
+        print(Style.RESET_ALL)
+
+        if char_input == 1:
+            dec_to_bin()
+        elif char_input == 2:
+            bin_to_dec()
+        elif char_input == 3:
+            xor_cipher()
+        elif char_input == 0:
+            break
+    except ValueError:
+        print(Fore.RED + "You should type a number!")
+        print(Style.RESET_ALL)
